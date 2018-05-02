923802ccdf9909399ae66bde401c890255576b2d
DESKTOP-40HIA6A\Agustí <ablasifl7@gmail.com>
2018-05-02 11:54:40 +0200

    Second commit

--- App2/src/main/java/net/cat/App2/App.java
+++ App2/src/main/java/net/cat/App2/App.java
@@ -8,7 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         if(args!=null){
         	for(String s:args){
         		System.out.println(s);
